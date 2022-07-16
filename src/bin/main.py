c5f2028ee869383821e1d639e063ada03642eac3
kristof01124 <kristof01124@gmail.com>
2022-07-16 13:46:05 +0200

    syncing

--- src/bin/main.py
+++ src/bin/main.py
@@ -13,9 +13,24 @@ starting_position = [
     -Board.Pieces.ROOK, -Board.Pieces.KNIGHT, -Board.Pieces.BISHOP, -Board.Pieces.QUEEN, -Board.Pieces.KING, -Board.Pieces.BISHOP, -Board.Pieces.KNIGHT, -Board.Pieces.ROOK
 ]
 
+test_position = [
+    0, 0, 0, 0, 0, Board.Pieces.KING, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, -Board.Pieces.PAWN, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, - Board.Pieces.QUEEN, 0, 0
+]
+
 def main():
     board = Board.Board()
     board.state = starting_position
-    board.generateDangerMap()
+    board.white = False
+    board.getAllPossibleMoves()
+    board.getDangerMap()
+    print(board.state)
+
 
 main()
--- src/includes/Chess/Board.py
+++ src/includes/Chess/Board.py
@@ -1,3 +1,6 @@
+import copy
+
+
 class Pieces:
     PAWN = 1
     KNIGHT = 2
@@ -19,8 +22,13 @@ class Moves:
     Bishop = [[0, 0], [0, 2], [2, 0], [2, 2]]
     Rook = [[0, 1], [1, 0], [1, 2], [2, 1]]
     King = list([j, i] for i in range(0, 3) for j in range(0,3, 2)) + [[1, 0], [1, 2]]
-    BlackPawn = [[0, 0], [0, 2]]
-    WhitePawn = [[2, 2], [2, 0]]
+    BlackPawnCapture = [[0, 0], [0, 2]]
+    BlackPawnAdvance = [0, 1]
+    BlackPawn = [[0, 0], [0, 1], [0, 2]]
+    WhitePawnCapture = [[2, 2], [2, 0]]
+    WhitePawn = [[2, 0], [2, 1], [2, 2]]
+    WhitePawnAdvance = [2, 1]
+
 
 class Inside:
     inside = [
@@ -53,11 +61,13 @@ class KnightMoves:
 
 class Board:
     def __init__(self):
+        self.__possibleMoves = []
         self.__dangerMap = [0 for i in range(0,64)]
         self.state = [0 for i in range(0, 64)]
         self.__castling = [False, False, False, False]
         self.__moveHistory = []
         self.white = True
+        self.__checks = 0
 
     #public
 
@@ -68,9 +78,82 @@ class Board:
         pass
 
     def getAllPossibleMoves(self):
-        pass
+        self.generateDangerMap()
+        for i in range(0, 64):
+            if self.withMe(i):
+                match abs(self.state[i]):
+                    case Pieces.KING:
+                        self.__getPossibleKingMoves(i)
+                    case Pieces.QUEEN:
+                        self.__getPossibleQueenMoves(i)
+                    case Pieces.BISHOP:
+                        self.__getPossibleBishopMoves(i)
+                    case Pieces.KNIGHT:
+                        self.__getPossibleKnightMoves(i)
+                    case Pieces.ROOK:
+                        self.__getPossibleRookMoves(i)
+                    case Pieces.PAWN:
+                        self.__getPossiblePawnMoves(i)
+        print(len(self.__possibleMoves))
 
     #private
+
+    def __getPossibleKingMoves(self, poz):
+        for j in Moves.King:
+            if Inside.inside[poz][j[0]][j[1]] and self.__dangerMap[poz + Directions.directions[j[0]][j[1]]] == 0 and not self.withMe(poz + Directions.directions[j[0]][j[1]]):
+                self.__possibleMoves.append([poz, poz + Directions.directions[j[0]][j[1]]])
+
+
+    def __getPossibleKnightMoves(self, poz):
+        for i in range(0, 8):
+            if KnightMoves.inside[poz][i] and not self.withMe(poz + KnightMoves.directions[i]):
+                self.__possibleMoves.append([poz, poz + KnightMoves.directions[i]])
+
+    def __getPossibleBishopMoves(self, poz):
+        for dir in Moves.Bishop:
+            self.__addSlideToPossibleMoves(poz, dir)
+
+    def __getPossibleQueenMoves(self, poz):
+        self.__getPossibleBishopMoves(poz)
+        self.__getPossibleRookMoves(poz)
+
+    def __getPossibleRookMoves(self, poz):
+        for dir in Moves.Rook:
+            self.__addSlideToPossibleMoves(poz, dir)
+
+    def __getPossiblePawnMoves(self, poz):
+        if self.white:
+            capture = Moves.WhitePawnCapture
+            advance = Moves.WhitePawnAdvance
+        else:
+            capture = Moves.BlackPawnCapture
+            advance = Moves.BlackPawnAdvance
+        for i in capture:
+            if Inside.inside[poz][i[0]][i[1]] and self.withEnemey(poz + Directions.directions[i[0]][i[1]]):
+                self.__possibleMoves.append([poz, poz + Directions.directions[i[0]][i[1]]])
+        if Inside.inside[poz][advance[0]][advance[1]] and self.state[poz + Directions.directions[advance[0]][advance[1]]] == 0:
+            self.__possibleMoves.append([poz, poz + Directions.directions[advance[0]][advance[1]]])
+            if Inside.inside[poz + Directions.directions[advance[0]][advance[1]]][advance[0]][advance[1]] and \
+                self.state[poz + Directions.directions[advance[0]][advance[1]] + Directions.directions[advance[0]][advance[1]]] == 0:
+                self.__possibleMoves.append([poz, poz + Directions.directions[advance[0]][advance[1]] + Directions.directions[advance[0]][advance[1]]])
+
+
+
+    def __getCastleMoves(self, poz):
+        pass
+
+    def __getEnPassantMoves(self, poz):
+        pass
+
+    def __addSlideToPossibleMoves(self, poz : int, dir):
+        move = Directions.directions[dir[0]][dir[1]]
+        basePoz = copy.copy(poz)
+        while Inside.inside[poz][dir[0]][dir[1]]:
+            poz += move
+            if self.state[poz] != 0:
+                return
+            self.__possibleMoves.append([basePoz, poz])
+
 
     #helper functions
 
@@ -89,6 +172,7 @@ class Board:
     #danger map generation
 
     def generateDangerMap(self):
+        self.__checks = 0
         for i in self.__dangerMap:
             i = 0
         for i in range(0, 64):
@@ -110,48 +194,79 @@ class Board:
                     case Pieces.QUEEN:
                         self.__dangerMapQueen(i)
 
-
-
-
+    def getDangerMap(self):
+        for i in range(0, 8):
+            print(self.__dangerMap[i * 8: (i+1) * 8])
 
     def __dangerMapHandleSliding(self, poz, dir):
         move = Directions.directions[dir[0]][dir[1]]
-        pinnedPiece = 0
+        pinnedPiece = -1
         while Inside.inside[poz][dir[0]][dir[1]]:
             poz += move
             if self.withMe(poz):
                 if pinnedPiece > -1:
                     if abs(self.state[poz]) == Pieces.KING:
-                        self.__handlePin(pinnedPiece, [2 - dir[0], 2 - dir[1]])
-                    else:
-                        return
+                        self.__handlePin(pinnedPiece, dir)
+                    return
+                if abs(self.state[poz]) == Pieces.KING:
+                    self.__handleCheck(poz, [2 - dir[0], 2 - dir[1]])
+                    return
                 pinnedPiece = poz
-            if self.withEnemey(poz):
-                return
             if pinnedPiece > -1:
-                return
+                continue
             if self.__dangerMap[poz] == 0:
                 self.__dangerMap[poz] = 1
+            if self.withEnemey(poz):
+                return
 
-    def __handlePin(self, poz, dir):
+
+    def __handleCheck(self, poz, dir):
+        self.__checks += 1
         move = Directions.directions[dir[0]][dir[1]]
-        poz += move
-        while not self.withMe(poz):
-            self.__dangerMap[poz] = 2
+        while Inside.inside[poz][dir[0]][dir[1]]:
             poz += move
+            if self.state[poz] != 0:
+                return
+            self.__dangerMap[poz] = 2
 
+    def __handlePin(self, poz, dir): #this function shouldn't be called to many times, so its fine to be kinda inofficient
+        possibleSlides = []
+        match abs(self.state[poz]):
+            case Pieces.ROOK:
+                if abs(dir[0] - 1) + abs(dir[1] - 1) == 1:
+                    possibleSlides = [dir, [2 - dir[0], 2 - dir[1]]]
+            case Pieces.BISHOP:
+                if abs(dir[0] - 1) + abs(dir[1] -1) == 2:
+                    possibleSlides = [dir, [2 - dir[0], 2 - dir[1]]]
+            case Pieces.QUEEN:
+                possibleSlides = [dir, [2 - dir[0], 2 - dir[1]]]
+            case Pieces.PAWN:
+                self.__handlePinnedPawn(poz, dir) #super rare case
+        for i in possibleSlides:
+            self.__addSlideToPossibleMoves(poz, i)
+        self.state[poz] += 10
+
+
+    def __handlePinnedPawn(self, poz, dir):
+        if dir[0] == 1:
+            return
+        if self.white and dir[0] == 0 or not self.white and dir[0] == 2:
+            dir = [2 - dir[0],2 - dir[1]]
+        if Inside.inside[poz][dir[0]][dir[1]]:
+            self.__possibleMoves.append([poz, poz + Directions.directions[dir[0]][dir[1]]])
+        # TODO: fix this shit
 
     def __dangerMapBishop(self, poz):
         for i in Moves.Bishop:
-            self.__dangerMapHandleSliding(poz, i)
+            self.__dangerMapHandleSliding(copy.copy(poz), i)
 
     def __dangerMapRook(self, poz):
         for i in Moves.Rook:
-            self.__dangerMapHandleSliding(poz, i)
+            self.__dangerMapHandleSliding(copy.copy(poz), i)
 
     def __dangerMapQueen(self, poz):
         self.__dangerMapBishop(poz)
-        self.__dangerMapBishop(poz)
+        self.__dangerMapRook(poz)
 
     def __dangerMapKing(self, poz):
         for i in Moves.King:
@@ -161,9 +276,9 @@ class Board:
 
     def __dangerMapPawn(self, poz):
         if not self.white:
-            l = Moves.WhitePawn
+            l = Moves.WhitePawnCapture
         else:
-            l = Moves.BlackPawn
+            l = Moves.BlackPawnCapture
         for j in l:
             if Inside.inside[poz][j[0]][j[1]]:
                 self.__dangerMap[poz + Directions.directions[j[0]][j[1]]] = 1
@@ -171,7 +286,7 @@ class Board:
     def __dangerMapKnight(self, poz):
         for i in range(0, 8):
             if KnightMoves.inside[poz][i]:
-                self.state[poz + KnightMoves.directions[[i]]] = 1
+                self.__dangerMap[poz + KnightMoves.directions[i]] = 1
 
 
 
